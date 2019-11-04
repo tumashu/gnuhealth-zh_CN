a30afc6f15a629236ee8e8b822f773cc2d6de94a
Feng Shu <tumashu@163.com>
2019-11-04 08:49:27 +0800

    update

--- push.sh
+++ push.sh
@@ -2,3 +2,4 @@
 
 scp -r ./GNUHEALTH/* gnuhealth@172.16.0.109:/home/gnuhealth/gnuhealth/tryton/server/modules/
 scp -r ./TRYTON/* gnuhealth@172.16.0.109:/home/gnuhealth/gnuhealth/tryton/server/modules/
+scp -r ./TRYTON-core/* gnuhealth@172.16.0.109:/home/gnuhealth/gnuhealth/tryton/server/trytond-5.0.14/trytond
