14f57e54748f4005514f7e8e96eb940f2433905e
Feng Shu <tumashu@163.com>
2020-07-03 08:44:36 +0800

    u

--- push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-scp -r ./GNUHEALTH/* gnuhealth@172.16.0.109:/home/gnuhealth/gnuhealth/tryton/server/modules/
-scp -r ./TRYTON/* gnuhealth@172.16.0.109:/home/gnuhealth/gnuhealth/tryton/server/modules/
-# scp -r ./TRYTON-core/* gnuhealth@172.16.0.109:/home/gnuhealth/gnuhealth/tryton/server/trytond-5.0.14/trytond
