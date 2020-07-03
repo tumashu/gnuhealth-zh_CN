c690e25708209237b2290c7feab903aabf1bd918
Feng Shu <tumashu@163.com>
2020-07-03 16:57:50 +0800

    u

--- update-trytond.sh
+++ update-trytond.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
 /usr/bin/cp -rf GNUHEALTH/* ~/gnuhealth/tryton/server/modules
+/usr/bin/cp -rf TRYTON/*    ~/gnuhealth/tryton/server/modules
 ~/gnuhealth/tryton/server/trytond-5.0.23/bin/trytond-admin --all -d gnuhealth
