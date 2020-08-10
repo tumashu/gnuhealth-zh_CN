a1a2bf4b6a739351825d7a0b01f13d12760873b9
Feng Shu <tumashu@163.com>
2020-08-10 09:56:59 +0800

    u

--- update-trytond.sh
+++ update-trytond.sh
@@ -4,7 +4,18 @@ source $HOME/.gnuhealthrc
 
 TRYTON_SERVER_DIR=${GNUHEALTH_DIR}/tryton/server
 
+echo "## Copy health-* translation files."
 /usr/bin/cp -rf GNUHEALTH/* ${TRYTON_SERVER_DIR}/modules
-/usr/bin/cp -rf TRYTON/*    ${TRYTON_SERVER_DIR}/trytond-${TRYTON_VERSION}/trytond/modules/
 
+TRYTON_DIR=$(pwd)/TRYTON
+
+cd ${TRYTON_DIR}
+for i in $(ls)         
+do  
+    echo "## copy $i translation file."
+    cd ${TRYTON_SERVER_DIR}/trytond-${TRYTON_VERSION}/trytond/modules/$i/
+    /usr/bin/cp -rf ${TRYTON_DIR}/${i}/*  ./;
+done  
+
+echo "## Run trytond-admin --all -d gnuhealth"
 ${TRYTON_SERVER_DIR}/trytond-${TRYTON_VERSION}/bin/trytond-admin --all -d gnuhealth
--- upload-to-weblate.sh
+++ upload-to-weblate.sh
@@ -2,46 +2,11 @@
 
 cd GNUHEALTH
 
-wlc  upload   gnu-health/health/zh_Hans/                                 --overwrite -i health/locale/zh_CN.po
-wlc  upload   gnu-health/health_archives/zh_Hans/                        --overwrite -i health_archives/locale/zh_CN.po
-wlc  upload   gnu-health/health_caldav/zh_Hans/                          --overwrite -i health_caldav/locale/zh_CN.po
-wlc  upload   gnu-health/health_calendar/zh_Hans/                        --overwrite -i health_calendar/locale/zh_CN.po
-wlc  upload   gnu-health/health_crypto/zh_Hans/                          --overwrite -i health_crypto/locale/zh_CN.po
-wlc  upload   gnu-health/health_crypto_lab/zh_Hans/                      --overwrite -i health_crypto_lab/locale/zh_CN.po
-wlc  upload   gnu-health/health_disability/zh_Hans/                      --overwrite -i health_disability/locale/zh_CN.po
-wlc  upload   gnu-health/health_ems/zh_Hans/                             --overwrite -i health_ems/locale/zh_CN.po
-wlc  upload   gnu-health/health_federation/zh_Hans/                      --overwrite -i health_federation/locale/zh_CN.po
-wlc  upload   gnu-health/health_genetics/zh_Hans/                        --overwrite -i health_genetics/locale/zh_CN.po
-wlc  upload   gnu-health/health_genetics_uniprot/zh_Hans/                --overwrite -i health_genetics_uniprot/locale/zh_CN.po
-wlc  upload   gnu-health/health_gyneco/zh_Hans/                          --overwrite -i health_gyneco/locale/zh_CN.po
-wlc  upload   gnu-health/health_history/zh_Hans/                         --overwrite -i health_history/locale/zh_CN.po
-wlc  upload   gnu-health/health_icd10/zh_Hans/                           --overwrite -i health_icd10/locale/zh_CN.po
-wlc  upload   gnu-health/health_icd9procs/zh_Hans/                       --overwrite -i health_icd9procs/locale/zh_CN.po
-wlc  upload   gnu-health/health_icpm/zh_Hans/                            --overwrite -i health_icpm/locale/zh_CN.po
-wlc  upload   gnu-health/health_icu/zh_Hans/                             --overwrite -i health_icu/locale/zh_CN.po
-wlc  upload   gnu-health/health_imaging/zh_Hans/                         --overwrite -i health_imaging/locale/zh_CN.po
-wlc  upload   gnu-health/health_inpatient/zh_Hans/                       --overwrite -i health_inpatient/locale/zh_CN.po
-wlc  upload   gnu-health/health_inpatient_calendar/zh_Hans/              --overwrite -i health_inpatient_calendar/locale/zh_CN.po
-wlc  upload   gnu-health/health_insurance/zh_Hans/                       --overwrite -i health_insurance/locale/zh_CN.po
-wlc  upload   gnu-health/health_iss/zh_Hans/                             --overwrite -i health_iss/locale/zh_CN.po
-wlc  upload   gnu-health/health_lab/zh_Hans/                             --overwrite -i health_lab/locale/zh_CN.po
-wlc  upload   gnu-health/health_lifestyle/zh_Hans/                       --overwrite -i health_lifestyle/locale/zh_CN.po
-wlc  upload   gnu-health/health_mdg6/zh_Hans/                            --overwrite -i health_mdg6/locale/zh_CN.po
-wlc  upload   gnu-health/health_ntd_chagas/zh_Hans/                      --overwrite -i health_ntd_chagas/locale/zh_CN.po
-wlc  upload   gnu-health/health_ntd_dengue/zh_Hans/                      --overwrite -i health_ntd_dengue/locale/zh_CN.po
-wlc  upload   gnu-health/health_nursing/zh_Hans/                         --overwrite -i health_nursing/locale/zh_CN.po
-wlc  upload   gnu-health/health_ophthalmology/zh_Hans/                   --overwrite -i health_ophthalmology/locale/zh_CN.po
-wlc  upload   gnu-health/health_orthanc/zh_Hans/                         --overwrite -i health_orthanc/locale/zh_CN.po
-wlc  upload   gnu-health/health_pediatrics/zh_Hans/                      --overwrite -i health_pediatrics/locale/zh_CN.po
-wlc  upload   gnu-health/health_pediatrics_growth_charts/zh_Hans/        --overwrite -i health_pediatrics_growth_charts/locale/zh_CN.po
-wlc  upload   gnu-health/health_pediatrics_growth_charts_who/zh_Hans/    --overwrite -i health_pediatrics_growth_charts_who/locale/zh_CN.po
-wlc  upload   gnu-health/health_profile/zh_Hans/                         --overwrite -i health_profile/locale/zh_CN.po
-wlc  upload   gnu-health/health_qrcodes/zh_Hans/                         --overwrite -i health_qrcodes/locale/zh_CN.po
-wlc  upload   gnu-health/health_reporting/zh_Hans/                       --overwrite -i health_reporting/locale/zh_CN.po
-wlc  upload   gnu-health/health_services/zh_Hans/                        --overwrite -i health_services/locale/zh_CN.po
-wlc  upload   gnu-health/health_services_lab/zh_Hans/                    --overwrite -i health_services_lab/locale/zh_CN.po
-wlc  upload   gnu-health/health_socioeconomics/zh_Hans/                  --overwrite -i health_socioeconomics/locale/zh_CN.po
-wlc  upload   gnu-health/health_stock/zh_Hans/                           --overwrite -i health_stock/locale/zh_CN.po
-wlc  upload   gnu-health/health_surgery/zh_Hans/                         --overwrite -i health_surgery/locale/zh_CN.po
-wlc  upload   gnu-health/health_webdav3_server/zh_Hans/                  --overwrite -i health_webdav3_server/locale/zh_CN.po
-wlc  upload   gnu-health/health_who_essential_medicines/zh_Hans/         --overwrite -i health_who_essential_medicines/locale/zh_CN.po
+# wlc upload gnu-health/health_archives/zh_Hans/  --overwrite -i health_archives/locale/zh_CN.po
+
+for i in $(ls)
+do  
+    echo "## Run command: wlc upload gnu-health/$i/zh_Hans/ --overwrite -i $i/locale/zh_CN.po"
+    wlc upload gnu-health/$i/zh_Hans/ --overwrite -i $i/locale/zh_CN.po
+done  
+
