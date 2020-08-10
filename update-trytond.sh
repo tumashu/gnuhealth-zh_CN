52f53950fe93436ff69727e86e4c4f4f2aece292
Feng Shu <tumashu@163.com>
2020-08-10 09:42:04 +0800

    u

--- update-trytond.sh
+++ update-trytond.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 
 source $HOME/.gnuhealthrc
-/usr/bin/cp -rf GNUHEALTH/* ${GNUHEALTH_DIR}/tryton/server/modules
-/usr/bin/cp -rf TRYTON/*    ${GNUHEALTH_DIR}/tryton/server/trytond-$TRYTON_VERSION/trytond/modules/
 
-~/gnuhealth/tryton/server/trytond-5.0.25/bin/trytond-admin --all -d gnuhealth
+TRYTON_SERVER_DIR=${GNUHEALTH_DIR}/tryton/server
+
+/usr/bin/cp -rf GNUHEALTH/* ${TRYTON_SERVER_DIR}/modules
+/usr/bin/cp -rf TRYTON/*    ${TRYTON_SERVER_DIR}/trytond-${TRYTON_VERSION}/trytond/modules/
+
+${TRYTON_SERVER_DIR}/trytond-${TRYTON_VERSION}/bin/trytond-admin --all -d gnuhealth
