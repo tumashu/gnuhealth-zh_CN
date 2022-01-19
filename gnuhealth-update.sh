e2cedd0e463f71dc12acdf1755c97db994d556fe
Feng Shu <tumashu@163.com>
2022-01-19 09:50:51 +0800

    删除 TRYTON 和 TRYTON-core 目录

--- gnuhealth-update.sh
+++ gnuhealth-update.sh
@@ -7,15 +7,5 @@ TRYTON_SERVER_DIR=${GNUHEALTH_DIR}/tryton/server
 echo "## Copy health-* translation files."
 /usr/bin/cp -rf GNUHEALTH/* ${TRYTON_SERVER_DIR}/modules
 
-TRYTON_DIR=$(pwd)/TRYTON
-
-cd ${TRYTON_DIR}
-for i in $(ls)         
-do
-    echo "## copy $i translation file."
-    cd ${TRYTON_SERVER_DIR}/trytond-${TRYTON_VERSION}/trytond/modules/$i/
-    /usr/bin/cp -rf ${TRYTON_DIR}/${i}/*  ./;
-done
-
 echo "## Run trytond-admin --all --language zh_CN -d gnuhealth"
 ${TRYTON_SERVER_DIR}/trytond-${TRYTON_VERSION}/bin/trytond-admin --all --language zh_CN -d gnuhealth
