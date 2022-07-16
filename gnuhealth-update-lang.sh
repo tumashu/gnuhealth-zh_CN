b8378d5c9529c9c79c80a8835ee7691481f8e804
Feng Shu <tumashu@163.com>
2022-07-16 14:01:36 +0800

    Add gnuhealth-update-lang.sh

---
+++ gnuhealth-update-lang.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+source $HOME/.gnuhealthrc
+
+TRYTON_SERVER_DIR=${GNUHEALTH_DIR}/tryton/server
+
+echo "## Copy health-* translation files."
+/usr/bin/cp -rf GNUHEALTH/* ${TRYTON_SERVER_DIR}/modules
+
+echo "## Run trytond-admin --all --language zh_CN -d gnuhealth"
+${TRYTON_SERVER_DIR}/trytond-${TRYTON_VERSION}/bin/trytond-admin --all --language zh_CN -d gnuhealth1
